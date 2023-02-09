d6044ebeab46371120c79c7d6b0b4091ac9268f3
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-09 14:55:42 +0000

    Added basic structure to run_tests py file and test file

---
+++ run_tests.py
@@ -0,0 +1,5 @@
+import unittest
+from tests.test_seatfinder import SeatFinderTest
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_seatfinder.py
@@ -0,0 +1,6 @@
+import unittest
+
+from src.seatfinder import SeatFinder
+
+class SeatFinderTest(unittest.TestCase):
+    pass
