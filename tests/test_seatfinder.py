eb78d8ff5ff073ed992d6e42b3b1c0792581bf71
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-09 15:00:21 +0000

    Added first test

--- tests/test_seatfinder.py
+++ tests/test_seatfinder.py
@@ -3,4 +3,12 @@ import unittest
 from src.seatfinder import SeatFinder
 
 class SeatFinderTest(unittest.TestCase):
-    pass
+    
+    def test_prefer_near_the_front(self):
+        finder = SeatFinder(available_seats={"A6", "B6", "C7"})
+        seats = finder.find_seats(1)
+        assert seats == {"A6"}
+
+    
+
+    
