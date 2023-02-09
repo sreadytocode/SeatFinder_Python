e92197c6122f3f408603694bca86c98d831db6d2
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-09 15:37:16 +0000

    Added unit test skips to skip rest of tests for now

--- tests/test_seatfinder.py
+++ tests/test_seatfinder.py
@@ -9,21 +9,25 @@ class SeatFinderTest(unittest.TestCase):
         seats = finder.find_seats(1)
         assert seats == {"A6"}
 
+    @unittest.skip("WIP")
     def test_finds_adjacent_seats_when_more_than_one_requested(self):
         finder = SeatFinder(available_seats={"A6", "A8", "C6", "C7"})
         seats = finder.find_seats(2)
         assert seats == {"C6", "C7"}
 
+    @unittest.skip("WIP")
     def test_finds_separate_seats_when_adjacent_not_available(self):
         finder = SeatFinder(available_seats={"A6", "B6", "C7"})
         seats = finder.find_seats(2)
         assert seats == {"B6", "A6"}
 
+    @unittest.skip("WIP")
     def test_find_seats_fails_when_not_enough_available(self):
         finder = SeatFinder(available_seats={"A6", "B6", "C7"})
         seats = finder.find_seats(5)
         assert seats == {}
 
+    @unittest.skip("WIP")
     def test_find_seats_for_wheelchair_users(self):
         finder = SeatFinder(available_seats={"A1W", "A6", "B6", "C7"})
         seats = finder.find_seats(1, wheelchair_count=1) 
