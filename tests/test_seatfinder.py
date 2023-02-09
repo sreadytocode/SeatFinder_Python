387eba4c3fc418adfb8f3d81eaec14b8ad21bb4f
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-09 15:02:49 +0000

    Added second test

--- tests/test_seatfinder.py
+++ tests/test_seatfinder.py
@@ -9,6 +9,13 @@ class SeatFinderTest(unittest.TestCase):
         seats = finder.find_seats(1)
         assert seats == {"A6"}
 
+    def test_finds_adjacent_seats_when_more_than_one_requested(self):
+        finder = SeatFinder(available_seats={"A6", "A8", "C6", "C7"})
+        seats = finder.find_seats(2)
+        assert seats == {"C6", "C7"}
+
+    
+
     
 
     
